57b38fbd2bb73ce9c1ee2bf2078dff563330485e
Dharan <14145706+dhrn@users.noreply.github.com>
2021-04-21 17:55:26 +0530

    Update protractor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -158,7 +158,6 @@ exports.config = {
                 '--disable-web-security',
                 '--disable-browser-side-navigation',
                 '--allow-running-insecure-content',
-                '--disable-dev-shm-usage',
                 ...(BROWSER_RUN === true ? [] : ['--headless'])]
         }
     },
